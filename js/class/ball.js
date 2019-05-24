13291b113c32a5371f46c40758f5ae8f9d2974cd
Kiyoshi <Donokami@users.noreply.github.com>
2019-05-24 12:53:36 +0200

    Var to const 2

--- js/class/ball.js
+++ js/class/ball.js
@@ -1,11 +1,11 @@
-const ballMinVel = 15;
-const ballMaxVel = 150;
-const ballMaxFactorVel = 2;
+var ballMinVel = 15;
+var ballMaxVel = 150;
+var ballMaxFactorVel = 2;
 
 class Ball {
 
 	constructor(x, y) {
-			
+
 		this.ball = game.add.sprite(x, y, 'ball');
 		game.physics.enable(this.ball);
 		this.ball.anchor.setTo(0.5, 0.5);
@@ -13,7 +13,7 @@ class Ball {
 		this.ball.body.setCircle(20);
 		this.ball.body.mass = 0.5;
 		this.ball.body.bounce.y = 0.5;
-		this.ball.body.collideWorldBounds = true;		
+		this.ball.body.collideWorldBounds = true;
 		this.ball.body.gravity.y = plyGravity;
 	}
 
@@ -21,12 +21,12 @@ class Ball {
 
 		 game.physics.arcade.collide(this.ball, platforms);
 		 game.physics.arcade.collide(this.ball, toitsGoals);
-		
-		for (var i=0; i < players.length; i++)		
-			game.physics.arcade.overlap(this.ball, players[i].player, this.playerHit, null, this);	
-			
-		for (var i=0; i < goals.length; i++)		
-			game.physics.arcade.collide(this.ball, goals[i].goal, this.goalHit, null, this);	
+
+		for (var i=0; i < players.length; i++)
+			game.physics.arcade.overlap(this.ball, players[i].player, this.playerHit, null, this);
+
+		for (var i=0; i < goals.length; i++)
+			game.physics.arcade.collide(this.ball, goals[i].goal, this.goalHit, null, this);
 
 		if (this.ball.body.y > game.height - 180)
 			return;
@@ -37,7 +37,7 @@ class Ball {
 			this.ball.body.velocity.x -= 10;
 	}
 
-	
+
 	playerHit (ball, player) {
 		// la balle part à 45degrees donc vel x = vel y
 		var vel = Math.abs(player.body.velocity.x) * ballMaxFactorVel;
@@ -48,17 +48,17 @@ class Ball {
 			vel = ballMaxVel;
 
 		var dir = ball.body.x - player.body.x;
-		
+
 		if ((dir > 0 && ball.body.velocity.x < 0) || (dir < 0 && ball.body.velocity.x > 0))
 			ball.body.velocity.x = 0;
 
 		ball.body.velocity.x += vel * Math.sign(dir);
 		ball.body.velocity.y = -vel * 3;
 	}
-	
+
 	goalHit (ball, goal) {
 		console.log("La balle est rentré dans un goal d'id #" + goal.id);
-		
+
 		if (goal.id == 0)
 			scoreBlue++;
 		else
